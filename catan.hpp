30348784a39587bc47be87a49294adcd4fec2b68
erik <110602802+erikidelchik@users.noreply.github.com>
2024-06-14 20:29:08 +0300

    Add files via upload

---
+++ catan.hpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "player.hpp"
+#include "board.hpp"
+#include "deck.hpp"
+
+namespace ariel {
+
+    class Catan {
+    private:
+
+        Deck deck;
+        vector<Player*> players;
+
+    public:
+
+        Catan(Player &p1,Player &p2, Player &p3){
+            players.push_back(&p1);
+            players.push_back(&p2);
+            players.push_back(&p3);
+        }
+
+        string getCardFromDeck(){
+            return deck.getCard();
+        }
+
+        Board getBoard(){
+            Board b;
+            return b;
+        }
+
+        vector<Player*>& getPlayers(){
+            return players;
+        }
+
+        void printStats(){
+            for(Player* player:players){
+                player->printPoints();
+            }
+        }
+
+    };
+
+
+}
